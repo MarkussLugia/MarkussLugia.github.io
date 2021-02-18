a371dc4ba66f93b1b9caeec3a3117f1cafa25eaa
Markuss <markusslugia@gmail.com>
2021-02-19 01:15:04 +0800

    bugfixes

--- fireworks.js
+++ fireworks.js
@@ -7,9 +7,9 @@ function fireworkClassic(view, x, y, sectionCount, offsetAngle, dotWidth, red, g
   sectionAngle = 360 / sectionCount;
   offsetAngle = (typeof offsetAngle == 'number') ?  offsetAngle : Math.random() * 360;
   dotWidth = (typeof dotWidth == 'number') ?  dotWidth : Math.random() * 20 + 5;
-  red = (red <= 255 && red >= 0) ?  red : Math.random() * 230 + 25;
-  green = (green <= 255 && green >= 0) ?  green : Math.random() * 230 + 25;
-  blue = (green <= 255 && green >= 0) ?  green : Math.random() * 230 + 25;
+  red = (red <= 255 && red >= 0) ?  red : Math.random() * 200 + 55;
+  green = (green <= 255 && green >= 0) ?  green : Math.random() * 200 + 55;
+  blue = (blue <= 255 && blue >= 0) ?  blue : Math.random() * 200 + 55;
   clean(view);
   mainframe = document.createElement("DIV");
   mainframe.setAttribute("birth", Date.now());
