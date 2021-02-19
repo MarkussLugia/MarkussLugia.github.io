b2a20ce5ef25f590c4ae4247efbbd60d15e7998a
Markuss <markusslugia@gmail.com>
2021-02-20 05:48:22 +0800

    Migrate to WAAPI

--- fireworks.js
+++ fireworks.js
@@ -17,28 +17,66 @@ function fireworksClassic(view, x, y, sectionCount, offsetAngle, dotWidth, red, 
   mainframe.style.left = (x - 150) + "px";
   mainframe.style.top = (y - 150) + "px";
   view.appendChild(mainframe);
-  let frame;
-  let dot;
   let core = document.createElement("DIV");
   core.className = "fireworksClassicCore";
   core.style.borderColor = "rgb(" + red + "," + green + "," + blue + ")";
   mainframe.appendChild(core);
+  core.animate([
+    {width: "10px", height: "10px", borderRadius: "10px", borderWidth: "5px", opacity: 1, filter: "blur(0px)", transform: "translate(0px,0px)"},
+    {width: "80px", height: "80px", borderRadius: "80px", borderWidth: "25px", opacity: 0, filter: "blur(20px)", transform: "translate(-35px,-35px)"}
+  ],{
+    duration: 600,
+    easing: "ease-out",
+    fill: "forwards"
+  });
   let halo = document.createElement("DIV");
   halo.className = "fireworksClassicHalo";
   halo.style.borderColor = "rgb(" + red + "," + green + "," + blue + ")";
   mainframe.appendChild(halo);
+  halo.animate([
+    {width: "10px", height: "10px", borderRadius: "10px", borderWidth: "5px", opacity: 1, filter: "blur(5px)", transform: "translate(0px,0px)"},
+    {width: "250px", height: "250px", borderRadius: "250px", borderWidth: "35px", opacity: 0, filter: "blur(30px)", transform: "translate(-120px,-120px)"}
+  ],{
+    duration: 1000,
+    easing: "ease-out",
+    fill: "forwards"
+  });
+  let dot;
   for (let i = 0; i < sectionCount; i++) {
-    frame = document.createElement("DIV");
-    frame.className = "fireworksClassicDotFrame";
-    frame.style.transform = "rotate(" + (sectionAngle * i + offsetAngle) + "deg)";
     dot = document.createElement("DIV");
     dot.className = "fireworksClassicDot";
     dot.style.width = dotWidth + "px";
-    dot.style.left = 20 - dotWidth / 2 + "px";
+    dot.style.left = 150 - dotWidth / 2 + "px";
+    dot.style.transformOrigin = dotWidth / 2 + "px 0px";
     dot.style.borderRadius = Math.random() * dotWidth + "px " + Math.random() * dotWidth + "px " + Math.random() * dotWidth + "px " + Math.random() * dotWidth + "px";
     dot.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")";
-    frame.appendChild(dot);
-    mainframe.appendChild(frame);
+    mainframe.appendChild(dot);
+    dot.animate([
+      {transform: "rotate(" + (sectionAngle * i + offsetAngle) + "deg) translateY(-10px)"},
+      {transform: "rotate(" + (sectionAngle * i + offsetAngle) + "deg) translateY(150px)"}
+    ],{
+      duration: 1200,
+      easing: "ease-out",
+      fill: "forwards"
+    });
+    dot.animate([
+      {opacity: 1, filter: "blur(0px)"},
+      {opacity: 0.8, filter: "blur(0px)", offset:0.6},
+      {opacity: 0, filter: "blur(10px)"}
+    ],{
+      duration: 1200,
+      easing: "ease-out",
+      fill: "forwards"
+    });
+    dot.animate([
+      {height: "10px", borderBottomWidth: "10px"},
+      {height: "30px", borderBottomWidth: "12px", offset: 0.2},
+      {height: "0px", borderBottomWidth: "0px"}
+    ],{
+      duration: 1200,
+      easing: "ease-out",
+      fill: "forwards"
+    });
   }
 }
 
